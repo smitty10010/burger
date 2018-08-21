960f8af5413ac46bddd9b66bffc1372972a971b0
Robert <35673753+smitty10010@users.noreply.github.com>
2018-08-20 22:43:57 -0600

    added port listen

--- server.js
+++ server.js
@@ -12,3 +12,7 @@ var PORT = process.env.PORT || 3000;
 //==================================================
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
