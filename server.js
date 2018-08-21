a746132f36995e5f022aa87f6271c40ca3454f98
Robert <35673753+smitty10010@users.noreply.github.com>
2018-08-20 22:42:45 -0600

    added in Dependencies and set up express app

---
+++ server.js
@@ -0,0 +1,14 @@
+//Dependencies
+//===============================================
+var express = require('express');
+var bodyParser = require('body-parser');
+
+//Setting up Express
+//================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+//Setting up the Express App to handle data parsing
+//==================================================
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
